065bf2e7af526ff19e9d5c8ff9b127d36ab0a2bb
ultragrace <udechukwuakachukwu.com>
2023-05-04 02:13:07 +0100

    Setting the project up including folder structure

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "978px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: "#FF014F",
+        bgColor: "#dde1e7",
+        textColor: "#dde1e7",
+      },
+    },
   },
   plugins: [],
-}
+};
