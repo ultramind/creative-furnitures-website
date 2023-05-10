cebad3f1f6d379e7401ce1d664646ea351fd7b43
ultragrace <udechukwuakachukwu.com>
2023-05-10 22:48:25 +0100

    Done with everything on the WhyMe component

--- frontend/src/components/Cards/WhyUs.jsx
+++ frontend/src/components/Cards/WhyUs.jsx
@@ -5,29 +5,29 @@ import { SiSpringsecurity } from "react-icons/si";
 
 const WhyUs = () => {
   return (
-    <div className="px-[15%] my-16 flex flex-col justify-center items-center">
-      <div className="grow py-4 px-2 flex  space-x-6 items-center justify-center border-2 border-gray-300 md:flex-row">
+    <div className=" flex flex-row flex-wrap my-12 justify-center px-8 md:px-[15%]">
+      <div className="basis-64 grow shrink border-2 border-gray-300 p-4 flex justify-center items-center space-x-4">
         <GiTruck size={50} className="text-primary" />
         <div>
           <h2>Free Delivery</h2>
           <p>All over Nigeria</p>
         </div>
       </div>
-      <div className="grow py-4 px-2 flex space-x-6 items-center justify-center border-2 border-gray-300 border-x-0">
+      <div className="basis-64 grow shrink border-2 border-gray-300 p-4 flex justify-center items-center space-x-4 ">
         <GiTakeMyMoney size={50} className="text-primary" />
         <div>
           <h2>Free Delivery</h2>
           <p>30 days monry return</p>
         </div>
       </div>
-      <div className="grow py-4 px-2 flex space-x-6 items-center justify-center border-2 border-gray-300 border-r-0">
+      <div className="basis-64 grow shrink border-2 border-gray-300 p-4 flex justify-center items-center space-x-4">
         <MdOutlineSupportAgent size={50} className="text-primary" />
         <div>
           <h2>24/7 Online Support</h2>
           <p>Customer Support</p>
         </div>
       </div>
-      <div className="grow py-4 px-2 flex space-x-6 items-center justify-center border-2 border-gray-300">
+      <div className="basis-64 grow shrink border-2 border-gray-300 p-4 flex justify-center items-center space-x-4">
         <SiSpringsecurity size={50} className="text-primary" />
         <div>
           <h2>Payment Security</h2>
